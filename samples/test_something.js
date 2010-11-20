f36979550f6039a8666ade7d8768511ee994149b
Alexandre Zani <alexandre.zani@gmail.com>
2010-11-19 21:34:12 -0800

    Added assertEquals

--- samples/test_something.js
+++ samples/test_something.js
@@ -7,9 +7,21 @@ function TestDummy() {
     assert(false);
   }
 
-  this.stuff = "Hello";
+  this.test_EqualsTrue = function() {
+    assertEquals("hello", "hello");
+  }
 
+  this.test_EqualsFalse = function() {
+    assertEquals("hello", "hello2");
+  }
 
+  this.test_EqualsFalseBool = function() {
+    assertEquals(true, false);
+  }
+
+  this.test_EqualsFalseNum = function() {
+    assertEquals(3, 21);
+  }
 }
 
 RhinoTestEngine(new TestDummy(), "TestDummy");
